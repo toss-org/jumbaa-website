568737bd99c1d7dfd8ab581bed0b4ecbb700ac8a
tzniceguy <joeshimbi@gmail.com>
2025-02-02 06:35:20 +0300

    data about team members behind jumbaa

---
+++ lib/utils.ts
@@ -0,0 +1,22 @@
+export const teamMembers = [
+  {
+    profile: "/images/undraw-coding.svg",
+    name: "John Doe",
+    position: "Lead Designer",
+  },
+  {
+    profile: "/images/undraw-coding.svg",
+    name: "Jane Doe",
+    position: "Lead Backend Developer",
+  },
+  {
+    profile: "/images/undraw-coding.svg",
+    name: "John Smith",
+    position: "Lead Frontend Developer",
+  },
+  {
+    profile: "/images/undraw-coding.svg",
+    name: "Jane Smith",
+    position: "Systems Engineer",
+  },
+];
