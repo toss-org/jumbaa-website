7f2b12c79c46425233551792df1fd4d9ca7faef5
tzniceguy <joeshimbi@gmail.com>
2025-02-02 03:28:10 +0300

    adding hero section to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#1E9577] text-white">
       <Header />
+      <Hero />
     </div>
   );
 }
