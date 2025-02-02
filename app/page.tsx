582ac2d691d730fb369c07bee7f5d28a19279226
tzniceguy <joeshimbi@gmail.com>
2025-02-02 04:38:11 +0300

    removing header to the homepage
    adding homepage related components and transfering the header to the
    layout file for sharing it through all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Header from "@/components/header";
+import Details from "@/components/details-section";
+import Features from "@/components/features-section";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#1E9577] text-white">
-      <Header />
+    <div className="min-h-screen text-white px-6">
       <Hero />
+      <Details />
+      <Features />
     </div>
   );
 }
