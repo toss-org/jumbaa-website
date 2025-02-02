687d74826f928ffc4b61f38cfe9d0ab51aa691d8
tzniceguy <joeshimbi@gmail.com>
2025-02-02 04:39:14 +0300

    removing background color

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,7 +2,7 @@ import CountdownTimer from "./countdown-timer";
 
 export default function Hero() {
   return (
-    <section className="min-h-[70vh] flex flex-col justify-center items-center py-20">
+    <section className="min-h-[70vh] flex flex-col justify-center items-center py-20 text-gray-800">
       <div className="container mx-auto px-6 text-center">
         <h1 className="text-4xl md:text-6xl font-bold mb-6 tracking-tight">
           Jumbaa Awaits
